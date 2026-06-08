f26d4e0ed6fce44d8d7f1836783875dcac8f4c07
soup12h <soup12.h@gmail.com>
2026-06-08 02:11:32 -0400

    Delete plug directory

--- plug/globals.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
-M.WAYWALL_CONFIG_DIR = os.getenv("HOME") .. "/.config/waywall/"
-M.PLUG_CONFIG_DIR = os.getenv("HOME") .. "/.config/waywall/" -- Same as waywall config dir.
-
-return M
